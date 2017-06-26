2f78926ecf14fe626558d8326df5d03b56441acf
tiany900125 <542298219@qq.com>
2017-06-26 21:39:22 +0800

    most code implementation

---
+++ express/ejsWebApp/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: '<h1>Express</h1>'
+                          ,users:[{username: 'Wilson'},
+                                {username: 'Wilson Zhong'},
+                                {username: 'Zhong Wei'}] 
+            });
+});
+
+module.exports = router;
